d76a30bbb4fa576f769106bb2656bec7ccf42528
HanhNT <ngocquyhoang.93@gmail.com>
2016-05-17 16:10:27 +0700

    Users list device api

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,7 @@ Rails.application.routes.draw do
     get "devices" => "devices#show"
     post "devices" => "devices#create"
     post "devices/update_position" => "devices#update_position"
+    get "users/list" => "tours#users_list"
     get "tours" => "tours#show"
     post 'tours/join' => "tours#join"
   end
